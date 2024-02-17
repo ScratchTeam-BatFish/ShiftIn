c6331db6b1ee959ad072cd4bb783df0d30b89ee4
Jules Mai <155853886+julesdmai@users.noreply.github.com>
2024-02-17 13:47:37 -0800

    Jules/server.js (#12)
    * Working on port
    
    * Improved code readability on server.js
    
    * Implemented server file tree
    
    * Implemented skeleton shiftController
    
    * Implemented skeleton tokenController
    
    * Implemented skeleton userController
    
    * Implemented controller imports to server

---
+++ server/controllers/userController.js
@@ -0,0 +1,18 @@
+// Import
+
+const userController = {};
+
+// Token values
+
+// Create a user
+userController.createUser = (req, res, next) => {
+    return next();
+}
+
+// Verify a user
+userController.verifyUser = (req, res, next) => {
+    return next();
+}
+
+// Export userController
+module.exports = userController;
