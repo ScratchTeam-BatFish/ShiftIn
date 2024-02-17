168d610ac3f27e8ee821c44a780139cbe1d0c294
julesdmai <julesdmai@gmail.com>
2024-02-17 13:24:58 -0800

    Implemented controller imports to server

--- server/server.js
+++ server/server.js
@@ -6,41 +6,48 @@ const mongoose = require('mongoose');
 // Access to process.env variables - future
 
 // Import controllers
+const userController = require('./controllers/userController');
+const tokenController = require('./controllers/tokenController');
+const shiftController = require('./controllers/shiftController');
 
 
 // Port for server
 const PORT = 3000;
 
+
 // handle parsing request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 // Connect to MongoDB
+
+
 
 // handle requests for static files
 app.use(express.static(path.join(__dirname, 'build')));
 
-// handles loading the initial html page and the bundle.js files in the build folder
+// handles loading the initial html page
 app.get('/', (req, res) => {
   console.log('Request for INDEX.HTML received');
 
-  const path = path.join(__dirname, '../build/index.html');
+  const route = path.join(__dirname, '../build/index.html');
 
-  return res.sendFile(path);
+  return res.sendFile(route);
 })
 
 
-
+// handles the bundle.js files in the build folder
 app.get('/bundle.js', (req, res) => {
   console.log('Request for bundle.js received')
 
-  const path = path.join(__dirname, '../build/bundle.js');
+  const route = path.join(__dirname, '../build/bundle.js');
 
-  return res.sendFile(path);
+  return res.sendFile(route);
 })
 
 
 // start server listener
 app.listen(PORT, () => {
-    console.log(`Backend server app listening on port ${PORT}`)
-  })
+    console.log(`Backend server app listening on port ${PORT}`);
+  });
