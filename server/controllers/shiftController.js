ffe51b68ada26882c5c75c8c6fb9d523e03c461a
julesdmai <julesdmai@gmail.com>
2024-02-17 13:10:56 -0800

    Implemented skeleton shiftController

---
+++ server/controllers/shiftController.js
@@ -0,0 +1,12 @@
+const shiftController = {};
+
+shiftController.pickupShift = (req, res, next) => {
+    return next();
+}
+
+shiftController.dropShift = (req, res, next) => {
+    return next();
+}
+
+// Export shiftController
+module.exports = shiftController;
