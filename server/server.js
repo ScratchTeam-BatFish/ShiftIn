59b0c9f3278cc6c55b1069dd9b54aef046b528fd
julesdmai <julesdmai@gmail.com>
2024-02-17 13:03:02 -0800

    Improved code readability on server.js

--- server/server.js
+++ server/server.js
@@ -2,31 +2,45 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose');
+// Cors configuration - future
+// Access to process.env variables - future
+
+// Import controllers
 
 
-const port = 3000;
+// Port for server
+const PORT = 3000;
 
 // handle parsing request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
 
 // handle requests for static files
 app.use(express.static(path.join(__dirname, 'build')));
 
 // handles loading the initial html page and the bundle.js files in the build folder
 app.get('/', (req, res) => {
-  console.log('Request for INDEX.HTML received')
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  console.log('Request for INDEX.HTML received');
+
+  const path = path.join(__dirname, '../build/index.html');
+
+  return res.sendFile(path);
 })
+
+
 
 app.get('/bundle.js', (req, res) => {
   console.log('Request for bundle.js received')
-  res.sendFile(path.join(__dirname, '../build/bundle.js'), {
-  });
-});
+
+  const path = path.join(__dirname, '../build/bundle.js');
+
+  return res.sendFile(path);
+})
 
 
 // start server listener
-app.listen(port, () => {
-    console.log(`Backend server app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Backend server app listening on port ${PORT}`)
   })
