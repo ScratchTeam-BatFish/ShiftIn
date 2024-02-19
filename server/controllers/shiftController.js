6f6d23f83ce7bba6969424974f83af184c6184e5
julesdmai <julesdmai@gmail.com>
2024-02-18 23:47:24 -0800

    Working on shiftController.assignShift

--- server/controllers/shiftController.js
+++ server/controllers/shiftController.js
@@ -1,18 +1,16 @@
 //import shiftModel & userModel
 const Shift = require('../models/shiftModel.js');
-const Schedule = require('../models/shiftModel.js');
 const User = require('../models/userModel.js');
 
 
 const shiftController = {};
 
-// Create a shift
+// Get a logged in user's shifts
 shiftController.getShifts = async (req, res, next) => {
     console.log('GET request to /dashboard');
     console.log('req.body contains: ', req.body);
 
-    // res.body should have userId
-    const { userId } = req.body; // note: we have to decide where
+    const { userId } = req.body;
     console.log('userId: ', userId);
 
     // Save $$$ // Check for missing parameters
@@ -31,7 +29,7 @@ shiftController.getShifts = async (req, res, next) => {
 
         // persist the data
         res.locals.shiftsArray = shiftsArray;
-        console.log('storing shiftsArray on res.locals.shifts: ', res.locals.shifts);
+        console.log('storing shiftsArray on res.locals.shifts: ', res.locals.shiftsArray);
 
         // testing
         console.log(`${shiftsArray.length} shifts have been retrieved for user ${userId}`);
@@ -44,8 +42,56 @@ shiftController.getShifts = async (req, res, next) => {
             log: `shiftController.createShift: ERROR ${err}`,
             status: 400,
             message: {err: 'Error occurred in shiftController.createShift. Check server logs for more details.'}
+        });
+    }
+}
+
+
+// Assign (or create) a shift
+shiftController.assignShift = async (req, res, next) => {
+    console.log('POST request to /assign');
+    console.log('req.body contains: ', req.body);
+
+    // destructure variables
+    const { date, employee, available, userId } = req.body;
+
+    // check input parameters
+    if (!date) {
+        return next({
+            log: 'missing shift information parameters',
+            message: {err: 'Error occurred in shiftController.assignShift.'},
+            status: 400,
         })
     }
+
+    // Creating shift and storing in mongoDB
+    try {
+        console.log('querying database...');
+        const shiftInformation = await Shift.create({
+            date: date,
+            employee: employee,
+            available: available,
+            userId: userId
+        });
+        console.log('shift created and stored in database');
+
+        // persist the data
+        res.locals.shift = shiftInformation;
+        console.log('storing shift information on res.locals.shift: ', res.locals.shift)
+
+        // testing
+        console.log(`shift on ${res.locals.shift.date} created and assigned to ${res.locals.shift.employee}`);
+
+        // return next
+        console.log('exiting shiftController.assignShift');
+        return next();
+
+    } catch (err) {
+
+    }
+    
+    
+
 }
 
 
